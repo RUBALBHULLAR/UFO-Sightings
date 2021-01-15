ae7d4130ca2a0d0a5166d1386fda539274d35483
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2021-01-15 02:07:51 -0700

    Update app.js

--- UFO-level-2/StarterCode/static/js/app.js
+++ UFO-level-2/StarterCode/static/js/app.js
@@ -5,3 +5,8 @@ console.log(tableData);
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
+
+// UFO Sighting values for each column
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+});
