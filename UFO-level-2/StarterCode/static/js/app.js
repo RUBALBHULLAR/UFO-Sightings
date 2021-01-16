576c2ca062f23c0af9504bd97f27717e3272faac
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2021-01-16 10:24:20 -0700

    Update app.js

--- UFO-level-2/StarterCode/static/js/app.js
+++ UFO-level-2/StarterCode/static/js/app.js
@@ -54,13 +54,14 @@ button.on("click", function() {
     Object.entries(selections).forEach(function([key, value]) {
       console.log(key, value);
       // Append a cell to the row for each value
-
+      var cell = row.append("td");
+      cell.text(value);
 
     });
 
 
   });
 
-
+  
     
 });
